b1077b6f5514a08ab28edd5779caee28cb7ddcc0
brokenpip3 <brokenpip3@gmail.com>
2024-09-15 18:15:01 +0200

    debug

--- tests/tests_helper.bash
+++ tests/tests_helper.bash
@@ -1,5 +1,6 @@
 _tests_helper() {
     export BATS_LIB_PATH=${BATS_LIB_PATH:-"/usr/lib"}
+    echo $BATS_LIB_PATH
     bats_load_library bats-support
     bats_load_library bats-assert
     bats_load_library bats-file
